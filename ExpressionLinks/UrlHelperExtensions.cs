3ad93601a083cbd31a7112ce06c90d210db6a412
Chad Kimes <chad.kimes@iticentral.com>
2015-03-16 19:28:30 -0400

    Add extension for UrlHelper

---
+++ ExpressionLinks/UrlHelperExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq.Expressions;
+
+namespace System.Web.Mvc.ExpressionLinks
+{
+    public static class UrlHelperExtensions
+    {
+        public static string Action<TController>
+            ( this UrlHelper helper
+            , Expression<Action<TController>> actionExpr
+            , object routeValues = null
+            , string protocol = null
+            , string hostname = null
+            )
+            where TController : Controller
+        {
+            var parseValues = ExpressionParser.Parse(actionExpr);
+            parseValues.RouteValues.AddObject(routeValues);
+            return helper.Action(parseValues.ActionName, parseValues.ControllerName, parseValues.RouteValues, protocol, hostname);
+        }
+    }
+}
